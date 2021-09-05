e0561652f6586f7bd8564470e0cf5d15a6615cc3
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-09-04 17:54:20 -0700

    Drawing app complete

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pygame
+pygame.font.init()
 
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 1536
+HEIGHT = 801
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+FONT=pygame.font.SysFont('comicsans',40)
 
 WHITE = (255, 255, 255)
+
+# board = [[(255, 255, 255) for i in range(WIDTH)] for i in range(HEIGHT)]
+
+
+def draw(win: pygame.Surface):
+    # win.fill(WHITE)
+    # for h, i in enumerate(board):
+    #     for k, j in enumerate(i):
+    #         pygame.draw.line(WIN, j, (h, k), (h, k))
+
+    pygame.display.update()
 
 
 def main():
@@ -12,12 +25,13 @@ def main():
     mouse_btn_dwn = False
     clock = pygame.time.Clock()
     WIN.fill(WHITE)
+    erase=False
     radius = 10
     while run:
         clock.tick(100)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+            if event.type==pygame.QUIT:
+                run=False
                 break
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_btn_dwn = True
@@ -28,8 +42,11 @@ def main():
             radius -= 3
         if keys[pygame.K_UP]:
             radius += 3
+        if keys[pygame.K_e]:
+            erase=not erase
         if mouse_btn_dwn:
-            pygame.draw.circle(WIN, (0, 0, 0), pygame.mouse.get_pos(), radius)
+            pygame.draw.circle(WIN,(0,0,0) if not erase else (255,255,255),pygame.mouse.get_pos(),radius)
+        draw(WIN)
 
 
 if __name__ == '__main__':
