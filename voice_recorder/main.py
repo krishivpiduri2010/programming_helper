bacaa2765ea5d93f40b2ed1ad62774c70a8ccf61
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-09-05 16:57:36 -0700

    voice recorder done

--- voice_recorder/main.py
+++ voice_recorder/main.py
@@ -1,6 +1,12 @@
+from tkinter.simpledialog import askinteger
+from tkinter import *
 import pyaudio
 import wave
 
+win = Tk()
+file_name = Entry(win)
+Label(win, text='file name').pack()
+file_name.pack()
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
@@ -16,23 +22,30 @@ stream = p.open(format=FORMAT,
                 input=True,
                 frames_per_buffer=CHUNK)
 
-print("* recording")
 
-frames = []
+def record_voice(seconds):
+    WAVE_OUTPUT_FILENAME = file_name.get() + '.wav'
+    print("* recording")
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+    frames = []
 
-print("* done recording")
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("* done recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+seconds = askinteger('Input', 'How long do you want your recording to last (Seconds)')
+Button(win, text='Record', command=lambda :record_voice(seconds)).pack()
+mainloop()
