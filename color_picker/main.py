d064139e9adca684aaebec958a5609621c231d23
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-09-06 15:46:13 -0700

    color picker done

--- color_picker/main.py
+++ color_picker/main.py
@@ -4,7 +4,7 @@ root=Tk()
 def color():
     (color := colorchooser.askcolor())
     Label(root,text='rgb:'+str(color[0])).pack()
-    Label(root,text='hex:'+str(color[1])).pack()
+    Label(root,text='hex'+str(color[1])).pack()
     print((color))
 Button(root,text='pick color',command=color).pack()
 mainloop()
