bfabfd05e8abb084a1c624557d6937312b21023e
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-09-05 17:24:53 -0700

    color picker done

---
+++ color_picker/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import colorchooser
+root=Tk()
+def color():
+    (color := colorchooser.askcolor())
+    Label(root,text='rgb:'+str(color[0])).pack()
+    Label(root,text='hex:'+str(color[1])).pack()
+    print((color))
+Button(root,text='pick color',command=color).pack()
+mainloop()
